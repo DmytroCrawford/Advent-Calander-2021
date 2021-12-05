38fe7e518a6e342f4677d8d8f513512164f67146
Dmytroc <74217403+DmytroCrawford@users.noreply.github.com>
2021-12-05 03:21:35 +0000

    Completed Day 3 Star 1

--- main.py
+++ main.py
@@ -53,6 +53,46 @@ def main():
     else:
       None
   ans=horiz*vert
-  print(ans)
+
 
 main()
+
+#Day 3
+# Build a function which iderates through each position
+# And finds the occurences in each place
+# Then build a function which gens a new binary number
+# and another function which inverets that binary key
+# and one last funciton to multiiply 
+
+def day3():
+  def newGammRate(x):
+    arra = {}
+    for line in x:
+      rangefun = range(len(line))
+      for ran in rangefun:
+        if line[ran]=='1':
+          if str(ran) in arra:
+            arra[str(ran)]+=1
+          else:
+            arra[str(ran)]=1
+    def buildBinary():
+      gamma=''
+      epsilon=''
+      for ran in rangefun:
+        if arra[str(ran)]>500:
+          gamma+='1'
+          epsilon+='0'
+        else:
+          gamma+='0'
+          epsilon+='1'
+      print(int(gamma,2)*int(epsilon,2))
+    buildBinary()
+  acu=[]
+  with open('JustEvenMoreData.csv') as f:
+    reader=csv.reader(f)
+    for el in reader:
+      for t in el:
+        acu.append(t)
+  newGammRate(acu)
+  
+day3()
