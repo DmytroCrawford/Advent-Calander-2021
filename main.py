195f94ad3125d8fe0845d64aa3e748ae2669b47a
Dmytroc <74217403+DmytroCrawford@users.noreply.github.com>
2021-12-05 02:23:56 +0000

    Day 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 
+#day 1 Pt 1
 
 accum=[]
 with open('data.csv') as f:
@@ -17,6 +18,41 @@ def findNum():
   for ran in t:
     if accum[ran] > accum[ran-1]:
       out+=1
-  print(out)
+  
 
 findNum()
+
+
+#day 2 Pt 1
+
+acum=[]
+with open('MoreData.csv') as f:
+  reader=csv.reader(f)
+  for el in reader:
+    for t in el:
+      acum.append(t)
+
+
+
+def main():
+  horiz=0
+  vert=0
+  for i in acum:
+    if 'up' in i:
+      for t in i:
+        if t.isdigit():
+          vert-=int(t)
+    elif 'forward' in i:
+      for t in i:
+        if t.isdigit():
+          horiz+=int(t)
+    elif 'down' in i:
+      for t in i:
+        if t.isdigit():
+          vert+=int(t)
+    else:
+      None
+  ans=horiz*vert
+  print(ans)
+
+main()
